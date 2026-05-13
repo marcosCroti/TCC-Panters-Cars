fa75b1a7ceb565ee44d9ef287c3d2c602b45fe39
Catarin208 <catarina.massola@aluno.senai.br>
2026-05-13 11:01:18 -0300

    Tabela peça e usuários

--- first_data.sql
+++ first_data.sql
@@ -29,8 +29,25 @@
     ima_peca BLOB NOT NULL
     );
 
+    insert into pecas(ima_peca, nome_tipo, grupo_peca, id_pecas, quantidade_pecas, quantidade_pecas_no_dia, local_peca)
+    values('img1', 'Pistão','Motor e Transmissão','00000001', 0, 0, 'Setor 1'),
+    ('img2', 'Pastilhas', 'Freios', '00000002', 0, 0, 'Setor 1'), 
+    ('img3', 'Amortecedores', 'Suspensão e Direção', '00000003', 0, 0, 'Setor 2'), 
+    ('img4', 'Bateria', 'Elétrica', '00000004', 0, 0, 'Setor 2'), 
+    ('img5','Para-choques', 'Carroceria/Acabamento', '00000005', 0, 0, 'Setor 3'),
+    ('img6', 'Cinto de Segurança', 'Componentes de Segurança', '00000006', 0, 0, 'Setor 3');
+
     select * from first_data.pecas;
 
+
+    
+    insert into pecas(ima_peca, nome_tipo, grupo_peca, id_pecas, quantidade_pecas, quantidade_pecas_no_dia, local_peca)
+    values('img1', 'Pistão','Motor e Transmissão','00000001', 0, 0, 'Setor 1'),
+    ('img2', 'Pastilhas', 'Freios', '00000002', 0, 0, 'Setor 1'), 
+    ('img3', 'Amortecedores', 'Suspensão e Direção', '00000003', 0, 0, 'Setor 2'), 
+    ('img4', 'Bateria', 'Elétrica', '00000004', 0, 0, 'Setor 2'), 
+    ('img5','Para-choques', 'Carroceria/Acabamento', '00000005', 0, 0, 'Setor 3'),
+    ('img6', 'Cinto de Segurança', 'Componentes de Segurança', '00000006', 0, 0, 'Setor 3');
     
  
     select * FROM pecas;
