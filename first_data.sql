1443164ca7c589e53781d9ba49f579b5fb6240dc
Catarin208 <catarina.massola@aluno.senai.br>
2026-05-13 11:05:26 -0300

    ID

--- first_data.sql
+++ first_data.sql
@@ -3,7 +3,8 @@
     use first_data;
 
     CREATE TABLE usuarios (
-    CPF VARCHAR(200) VARCHAR PRIMARY KEY,
+    ID INT AUTO_INCREMENT UNIQUE PRIMARY KEY,
+    CPF VARCHAR(200) NOT NULL,
     usuario_nome VARCHAR(50) NOT NULL,
     email VARCHAR(200) NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
@@ -28,17 +29,6 @@
     id_pecas INT REFERENCES pecas(id_pecas),
     ima_peca BLOB NOT NULL
     );
-
-    insert into pecas(ima_peca, nome_tipo, grupo_peca, id_pecas, quantidade_pecas, quantidade_pecas_no_dia, local_peca)
-    values('img1', 'Pistão','Motor e Transmissão','00000001', 0, 0, 'Setor 1'),
-    ('img2', 'Pastilhas', 'Freios', '00000002', 0, 0, 'Setor 1'), 
-    ('img3', 'Amortecedores', 'Suspensão e Direção', '00000003', 0, 0, 'Setor 2'), 
-    ('img4', 'Bateria', 'Elétrica', '00000004', 0, 0, 'Setor 2'), 
-    ('img5','Para-choques', 'Carroceria/Acabamento', '00000005', 0, 0, 'Setor 3'),
-    ('img6', 'Cinto de Segurança', 'Componentes de Segurança', '00000006', 0, 0, 'Setor 3');
-
-    select * from first_data.pecas;
-
 
     
     insert into pecas(ima_peca, nome_tipo, grupo_peca, id_pecas, quantidade_pecas, quantidade_pecas_no_dia, local_peca)
