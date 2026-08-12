cbeecdd06629c2dff1e80710146cd0d79fbef21d
JoaoFernando-hub <joaofernandolb@gmail.com>
2026-08-12 14:51:10 -0300

    O grande save do Jão

--- auth/login.php
+++ auth/login.php
@@ -46,14 +46,16 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         }
     }
 }
+
 ?>
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AutoScanPro - Login</title>
-    <link rel="stylesheet" href="../Front-End/CSS/login.css">
+    <link rel="stylesheet" href="../Front-End/CSS/Style_Users/Login_User.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
 <body>
