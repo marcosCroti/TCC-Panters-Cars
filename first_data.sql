0064677dc5f40785be9eb05a986e775bad373f38
Catarina208 <catarina.massola@aluno.senai.br>
2026-08-19 16:19:48 -0300

    Banco atualizado

--- first_data.sql
+++ first_data.sql
@@ -34,12 +34,12 @@
 
     
     insert into pecas(ima_peca, nome_tipo, grupo_peca, id_pecas, quantidade_pecas, quantidade_pecas_no_dia, setor)
-    values('img1', 'Pistão','Motor e Transmissão','00000001', 0, 0, 'Setor 1' ),
-    ('img2', 'Pastilhas', 'Freios', '00000002', 0, 0, 'Setor 1'), 
-    ('img3', 'Amortecedores', 'Suspensão e Direção', '00000003', 0, 0, 'Setor 2'), 
-    ('img4', 'Bateria', 'Elétrica', '00000004', 0, 0, 'Setor 2'), 
-    ('img5','Para-choques', 'Carroceria/Acabamento', '00000005', 0, 0, 'Setor 3'),
-    ('img6', 'Cinto de Segurança', 'Componentes de Segurança', '00000006', 0, 0, 'Setor 3');
+    values('img1', 'Pistão','Motor e Transmissão','00000001', 0, 0, 'Motor e Transmissão' ),
+    ('img2', 'Pastilhas', 'Freios', '00000002', 0, 0, 'Setor 1', 0, 0, 'Freios'), 
+    ('img3', 'Amortecedores', 'Suspensão e Direção', '00000003', 0, 0, 'Suspensão  e Direção'), 
+    ('img4', 'Bateria', 'Elétrica', '00000004', 0, 0, 'Setor 2', 0,0, 'Elétrica'), 
+    ('img5','Para-choques', 'Carroceria/Acabamento', '00000005', 0, 0, 'Carroceria/Acabamento'),
+    ('img6', 'Cinto de Segurança', 'Componentes de Segurança', '00000006', 0, 0, 'Componentes de Segurança');
     
  
     select * FROM pecas;
@@ -58,11 +58,11 @@
  
  
     insert into funcionario(nome_fun, CPF, email, setor, quantidade_pecas_no_dia, status_fun)
-    values('João Silva', '111.111.111-11','joao@empresa.com', 'Montagem', '47', 'Ativo'),
-    ('Maria Santos', '222.222.222-22','maria@empresa.com', 'Qualidade', '35', 'Ativo'),
-    ('Carlos Ramos', '333.333.333-33','carlos@empresa.com', 'Expedição', '52', 'Ativo'),
-    ('Ana Gabriela', '444.444.444-44','ana@empresa.com', 'Inspeção', '28', 'Inativo'),
-    ('Pedro Pimenteira', '555.555.555-55','pedro@empresa.com', 'Montagem', '61', 'Ativo');
+    values('João Silva', '111.111.111-11','joao@empresa.com', 'Motor e Transmissão', '47', 'Ativo'),
+    ('Maria Santos', '222.222.222-22','maria@empresa.com', 'Freios', '35', 'Ativo'),
+    ('Carlos Ramos', '333.333.333-33','carlos@empresa.com', 'Suspensão  e Direção', '52', 'Ativo'),
+    ('Ana Gabriela', '444.444.444-44','ana@empresa.com', 'Elétrica', '28', 'Inativo'),
+    ('Pedro Pimenteira', '555.555.555-55','pedro@empresa.com', 'Componentes de Segurança', '61', 'Ativo');
 
     create table pecas_por_funcionario(
     nome_fun VARCHAR(200) NOT NULL,
@@ -77,4 +77,49 @@
     quantidade_pecas INT NOT NULL,
     quantidade_pecas_setor INT NOT NULL,
     quantidade_pecas_no_dia INT NOT NULL
+    );
+
+    create table pistao_parametro(
+        cor_parametro VARCHAR(200) NOT NULL, 
+        tamanho_parametro FLOAT NOT NULL, 
+        fixuras_parametro VARCHAR(200) NOT NULL, 
+        encaixe_parametro VARCHAR(200) NOT NULL,
+        espessura_parametro FLOAT NOT NULL
+    );
+
+    create table pastilha_parametro(
+        cor_parametro VARCHAR(200) NOT NULL, 
+        tamanho_parametro FLOAT NOT NULL,
+        fixuras_parametro VARCHAR(200) NOT NULL, 
+        encaixe_parametro VARCHAR(200) NOT NULL,
+        espessura_parametro FLOAT NOT NULL
+         
+    );
+
+    create table amortecedor_parametro(
+        cor_parametro VARCHAR(200) NOT NULL, 
+        tamanho_parametro FLOAT NOT NULL,
+        fixuras_parametro VARCHAR(200) NOT NULL, 
+        encaixe_parametro VARCHAR(200) NOT NULL,
+        espessura_parametro FLOAT NOT NULL
+    
+        
+    );
+
+    create table para_choque_parametro(
+        cor_parametro VARCHAR(200) NOT NULL, 
+        tamanho_parametro FLOAT NOT NULL, 
+        fixuras_parametro VARCHAR(200) NOT NULL, 
+        encaixe_parametro VARCHAR(200) NOT NULL,
+        espessura_parametro FLOAT NOT NULL
+        
+    );
+
+    create table bateria_parametro(
+        cor_parametro VARCHAR(200) NOT NULL, 
+        tamanho_parametro FLOAT NOT NULL, 
+        fixuras_parametro VARCHAR(200) NOT NULL, 
+        encaixe_parametro VARCHAR(200) NOT NULL, 
+        espessura_parametro FLOAT NOT NULL
+        
     );
